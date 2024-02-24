77510db8e63b8f0dc615ef904707c47fe94ec361
Khusanjon <developerkhusanjon@gmail.com>
2024-02-24 22:50:55 +0500

    course sub-projects all set up

---
+++ streamer-service/src/main/java/io/streamer/streamerservice/StreamerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.streamer.streamerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamerServiceApplication.class, args);
+    }
+
+}
---
+++ streamer-service/src/test/java/io/streamer/streamerservice/StreamerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.streamer.streamerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
